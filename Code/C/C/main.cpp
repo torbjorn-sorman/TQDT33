6dc9aa963cdb591a2c6c768362540341171335b9
torbjorn-sorman <torbjorn.sorman@gmail.com>
2015-08-07 16:44:27 +0200

    improved FFT

--- Code/C/C/main.cpp
+++ Code/C/C/main.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 
 #define M_2_PI 6.28318530718
-#define N 4096
+#define N 1024 // 73 times -> ~180
 
 typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
@@ -49,18 +49,15 @@ struct complex
     }
 };
 
-uint32_t
-reverse(uint32_t x, uint32_t l)
+uint32_t reverse(uint32_t x, uint32_t l)
 {
     x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
     x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
     x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
     x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
     return((x >> 16) | (x << 16)) >> (32 - l);
-
 }
 
-uint32_t nk_mod_n(uint32_t n, uint32_t b, uint32_t d);
 complex complex_pow(complex c, uint32_t p);
 complex pow_complex(complex c, uint32_t p);
 const int tab32[32] = { 0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30, 8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31 };
@@ -92,17 +89,30 @@ int main()
     }
     impulse[1].r = 1.0;
 
+    // TEST nk % N
+    for (int n = 0; n < N; ++n)
+    {
+        //printf("\n");
+        for (int k = 0; k < log2_32(N); ++k)
+        {
+            //printf("%d\t", ((n * k) % N));
+        }
+        //printf("\n");
+    }
+    //getchar();
+    //return 0;
     /* FFT */
     printf("\nWarm up FFT...\n");
-    fft(impulse, res_FFT);    
+    fft(impulse, res_FFT);
     printf("Running...\n");
-    QueryPerformanceCounter(&tStart);    
+    QueryPerformanceCounter(&tStart);
     fft(impulse, res_FFT);
     QueryPerformanceCounter(&tStop);
     printTime(tStart, tStop, freq);
     time_FFT = (double)(tStop.QuadPart - tStart.QuadPart) * 1000.0 / (double)freq.QuadPart;
 
     /* Naive DFT */
+
     printf("\nWarm up naive DFT...\n");
     naive_dft(impulse, res_NDFT);
     printf("Running...\n");
@@ -114,6 +124,7 @@ int main()
 
     //printResult(res, N, "impulse", verify_impulse(res, N));
     compareComplex(res_FFT, res_NDFT, time_FFT, time_NDFT);
+
     getchar();
     return 0;
 }
@@ -121,23 +132,28 @@ int main()
 /* Naive Discrete Fourier Transform, essentially as per definition */
 void naive_dft(complex *x, complex *X)
 {
-    complex sum = { 0.0, 0.0 };
+    double real, img;
     complex y = { 0.0, 0.0 };
+    double re, im;
     complex tmp = { 0.0, 0.0 };
     double theta = 1.0;
     double c1 = -M_2_PI / N;
     double c2 = 1.0;
     for (int k = 0; k < N; ++k)
     {
-        sum = { 0.0, 0.0 };
+        real = 0.0;
+        img = 0.0;
         c2 = c1 * k;
         for (int n = 0; n < N; ++n)
         {
             theta = c2 * n;
-            y = { cos(theta), sin(theta) };
-            sum += (x[n] * y);
+            re = cos(theta);
+            im = sin(theta);
+            real += x[n].r * re + x[n].i * im;
+            img += x[n].r * im + x[n].i * re;
         }
-        x[k] = sum;
+        x[k].r = real;
+        x[k].i = img;
     }
 }
 
@@ -155,28 +171,55 @@ void fft(complex *x, complex *X)
     complex tmp[N];
     const uint32_t depth = log2_32(N);
     const uint32_t n_half = N / 2;
-    complex W = { cos(-M_2_PI / N), sin(-M_2_PI / N) };
+    double w_angle = -M_2_PI / N;
+    double x_re, x_im, theta;
     uint32_t bit = 0;
-    complex upper, lower;
-    uint32_t u, l;
-    uint32_t dist, offset;    
+    double u_re, u_im, l_re, l_im;
+    uint32_t u, l, p;
+    uint32_t dist, dist_2, offset;
+
     for (uint32_t n = 0; n < N; ++n)
         tmp[n] = x[n];
+
     dist = N;
     for (uint32_t k = 0; k < depth; ++k)
     {
         bit = depth - 1 - k;
-        dist = (n_half >> k);
+        dist_2 = dist;
+        dist = dist >> 1;
         offset = 0;
         for (uint32_t n = 0; n < n_half; ++n)
         {
-            offset += (n >= (dist + offset)) * dist * 2;
+            offset += (n >= (dist + offset)) * dist_2;
             l = (n & ~(1 << bit)) + offset;
-            u = (n | 1 << bit) + offset;
-            lower = tmp[l] + complex_pow(W, nk_mod_n(l, bit, depth)) * tmp[u];
-            upper = tmp[l] + complex_pow(W, nk_mod_n(u, bit, depth)) * tmp[u];
-            tmp[l] = lower;
-            tmp[u] = upper;
+            u = l + dist;
+            // Lower
+            p = (l >> bit);
+            p = (((p & 0xaaaaaaaa) >> 1) | ((p & 0x55555555) << 1));
+            p = (((p & 0xcccccccc) >> 2) | ((p & 0x33333333) << 2));
+            p = (((p & 0xf0f0f0f0) >> 4) | ((p & 0x0f0f0f0f) << 4));
+            p = (((p & 0xff00ff00) >> 8) | ((p & 0x00ff00ff) << 8));            
+            theta = w_angle * (((p >> 16) | (p << 16)) >> (32 - depth));
+            x_re = cos(theta);
+            x_im = sin(theta);
+            l_re = tmp[l].r + x_re * tmp[u].r + x_im * tmp[u].i;
+            l_im = tmp[l].i + x_re * tmp[u].i + x_im * tmp[u].r;
+            // Upper
+            p = (u >> bit);
+            p = (((p & 0xaaaaaaaa) >> 1) | ((p & 0x55555555) << 1));
+            p = (((p & 0xcccccccc) >> 2) | ((p & 0x33333333) << 2));
+            p = (((p & 0xf0f0f0f0) >> 4) | ((p & 0x0f0f0f0f) << 4));
+            p = (((p & 0xff00ff00) >> 8) | ((p & 0x00ff00ff) << 8));
+            theta = w_angle * (((p >> 16) | (p << 16)) >> (32 - depth));
+            x_re = cos(theta);
+            x_im = sin(theta);
+            u_re = tmp[l].r + x_re * tmp[u].r + x_im * tmp[u].i;
+            u_im = tmp[l].i + x_re * tmp[u].i + x_im * tmp[u].r;
+            // Insert
+            tmp[l].r = l_re;
+            tmp[l].i = l_im;
+            tmp[u].r = u_re;
+            tmp[u].i = u_im;
         }
     }
     for (int n = 0; n < N; ++n)
@@ -211,25 +254,15 @@ int verify_impulse(complex *c, int size)
     return 1;
 }
 
-uint32_t nk_mod_n(uint32_t n, uint32_t b, uint32_t d)
-{
-    uint32_t x = (n >> b);
-    x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
-    x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
-    x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
-    x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
-    return((x >> 16) | (x << 16)) >> (32 - d);
-}
-
 int log2_32(uint32_t value)
 {
     value |= value >> 1; value |= value >> 2; value |= value >> 4; value |= value >> 8; value |= value >> 16;
     return tab32[(uint32_t)(value * 0x07C4ACDD) >> 27];
 }
 
-complex complex_pow(complex c, uint32_t p)
+complex complex_pow(double a, uint32_t p)
 {
-    double theta = acos(c.r) * p;
+    double theta = a * p;
     return{ cos(theta), sin(theta) };
 }
 
@@ -253,6 +286,6 @@ void compareComplex(complex *c1, complex *c2, double t1, double t2)
         }
     }
     printf("\n%s\n", res ? "EQUAL" : "NOT EQUAL");
-    printf("\nImproved %f times.", (t2 / t1));
-    printf("\nTheoretical limit %f times.", (((double)N * N) / ((double)N * log2_32(N))));
+    printf("\nImproved %d times.", (int)(t2 / t1));
+    printf("\nTheoretical limit %d times.", (int)(((double)N * N) / ((double)N * log2_32(N))));
 }
