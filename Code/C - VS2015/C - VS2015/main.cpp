dab74dd0ed76edf07eedd06971bceaf36e7d0eaf
torbjorn-sorman <torbjorn.sorman@gmail.com>
2015-08-10 07:47:17 +0200

    Small change to vs2015-project

--- Code/C - VS2015/C - VS2015/main.cpp
+++ Code/C - VS2015/C - VS2015/main.cpp
@@ -2,7 +2,19 @@
 #include <Windows.h>
 #include <cmath>
 
-#include "definitions.h"
+#define M_2_PI 6.28318530718
+#define N 8192 // 73 times -> ~180
+
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+
+struct complex
+{
+	float r;
+	float i;
+};
+
+const int tab32[32] = { 0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30, 8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31 };
 
 int log2_32(uint32_t value)
 {
